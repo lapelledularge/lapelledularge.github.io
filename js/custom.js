12bbf79e7a3254a50fc709b22f57c074ffdd8eb9
Mewen <mewenmichel@yahoo.fr>
2016-09-09 12:02:18 +0200

    Info animation working

--- js/custom.js
+++ js/custom.js
@@ -5,28 +5,14 @@
 	var infoClickInit = function() {
 		$('li.icon').on('click', function(event) {
 			event.preventDefault();
-			// var $this = $(this),
-			// 	data = $this.data('tab'),
-			// 	pie = $this.data('pie');
 
 			// add/remove active class
+			var anotherInfoIsSelected = !$(this).is($('li.active-info'))
 			$('li.icon').removeClass('active-info');
-			// $this.closest('li').addClass('active-info');
-			$(this).addClass('active-info');
 
-			// $('.icon.active').addClass('animated fadeOutDown');
-
-			// setTimeout(function(){
-			// 	$('.fh5co-tab-content.active').removeClass('active animated fadeOutDown fadeInUp');
-			// 	$('.fh5co-tab-content[data-content="'+data+'"]').addClass('animated fadeInUp active');
-			// 	getHeight();
-			// }, 500);
-
-			// if ( pie === 'yes' ) {
-			// 	setTimeout(function(){
-			// 		pieChart();
-			// 	}, 800);
-			// }
+			if(anotherInfoIsSelected){
+				$(this).addClass('active-info');
+			}
 			
 		})
 	};
