0dcf5521b25c1dede2de3b21be246139d1f68e85
Mewen <mewenmichel@yahoo.fr>
2016-09-11 19:00:18 +0200

    Added fullscreen slideshow

--- js/custom.js
+++ js/custom.js
@@ -7,6 +7,15 @@
 	var BGINTERVAL = 10000;
 	var FADEINTERVAL = BGINTERVAL/10;
 
+	var bgParams = {
+		i: 0,
+		max: -1,
+		j: 0
+	}
+
+	var backgroundInterval; // interval between 2 animations
+	var backgroundTimeout; // timeout to do smt after animation
+
 	var infoClickInit = function() {
 		$('li.icon').on('click', function(event) {
 			event.preventDefault();
@@ -23,31 +32,52 @@
 	};
 
 	var backgroundMng = function() {
+		startBgInterval();
+		lookForMax(bgParams);
+	}
 
-		var params = {
-			i: 1,
-			max: -1,
-			j: 0
-		}
+	var stopBgInterval = function(){
+		clearInterval(backgroundInterval)
+	}
 
-		setInterval(function(){
-			if (params.i >= params.max){
-				params.i = 0;
+	var startBgInterval = function(){
+		backgroundInterval = setInterval(function(){
+			nextBackground();
+		}, BGINTERVAL)
+	}
+
+	var previousBackground = function() {
+		bgParams.i--;
+
+		if (bgParams.max != -1) {
+			if (bgParams.i < 0){
+				bgParams.i = bgParams.max - 1;
 			}
 
-			$("#bgTemp").css('display', 'none');
-			$("#bgTemp").css('background-image', 'url(' + BGPREFIX + params.i + BGSUFFIX + ')');
-			$("#bgTemp").fadeIn(FADEINTERVAL);
+			setBg(bgParams.i);
+		};
+	}
 
-			setTimeout(function(){
-				$('body').css('background-image', 'url(' + BGPREFIX + params.i + BGSUFFIX + ')');
-				params.i++;
-			}, FADEINTERVAL);
+	var nextBackground = function() {
+		bgParams.i++;
 
-		}, BGINTERVAL)
+		if (bgParams.max != -1) {
+			if (bgParams.i >= bgParams.max){
+				bgParams.i = 0;
+			}
 
-		lookForMax(params);
+			setBg(bgParams.i);
+		};
+	}
 
+	var setBg = function(i) {
+		$("#bgTemp").css('display', 'none');
+		$("#bgTemp").css('background-image', 'url(' + BGPREFIX + i + BGSUFFIX + ')');
+		$("#bgTemp").fadeIn(FADEINTERVAL);
+
+		setTimeout(function(){
+			$('body').css('background-image', 'url(' + BGPREFIX + i + BGSUFFIX + ')');
+		}, FADEINTERVAL);
 	}
 
 	var lookForMax = function (bgParameters){
@@ -83,11 +113,55 @@
 		$("#email").text($("#email").text().replace("nawak", "@"))
 	}
 
+	var toogleBgExpand = function() {
+		$("#bgExpandCompress").on('click', function(){
+			if ($("#bgExpandCompress").hasClass("icon-expand")){ // if it's compressed
+				// Change icon
+				$("#bgExpandCompress").removeClass("icon-expand");
+				$("#bgExpandCompress").addClass("icon-compress");
+
+				// Expand header
+				$("#fh5co-main").css("margin-top", $(window).height())
+
+				// Show previous/next arrows
+				$(".icon-arrow-left").css("opacity", "1")
+				$(".icon-arrow-right").css("opacity", "1")
+
+				// Start background slideshow
+				stopBgInterval()
+
+			} else { // it's expanded, let's compress it
+				// Change icon
+				$("#bgExpandCompress").removeClass("icon-expand");
+				$("#bgExpandCompress").addClass("icon-compress");
+
+				// Compress header
+				$("#fh5co-main").css("margin-top", "5em")
+
+				// Hide previous/next arrows
+				$(".icon-arrow-left").css("opacity", "0")
+				$(".icon-arrow-right").css("opacity", "0")
+
+				// Stop background slideshow
+				startBgInterval()
+			}
+		})
+
+		$("#bgPrevious").on('click', function(){
+			previousBackground()
+		})
+
+		$("#bgNext").on('click', function(){
+			nextBackground()
+		})
+	}
+
 	// Document on load.
 	$(function(){
 		backgroundMng();
 		infoClickInit();
 		createEmailAdress();
+		toogleBgExpand();
 	});
 
 
